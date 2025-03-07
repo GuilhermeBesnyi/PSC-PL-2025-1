09bd6f1e327750be38ed357d8add2b630699fdba
Guilherme Besnyi Jose - 823127590 <823127590@ulife.com.br>
2025-03-07 11:33:26 -0300

    Crição de operadores na classe

--- Operadores.java
+++ Operadores.java
@@ -1,6 +1,39 @@
 public class Operadores{
 
     public static void main (String[] args){
-        System.out.println("Olá mundo")
+        //Operadores aritimeticos
+        int a = 10;
+        int b = 5;
+System.out.println (a + b);
+System.out.println (a - b);
+System.out.println (a / b);
+System.out.println (a * b);
+System.out.println (a % b);
+
+    int c = 20;
+    int d = 15;
+System.out.println (c == d);
+System.out.println (c |= d);
+System.out.println (c > d);
+System.out.println (c < d);
+System.out.println (c >= d);
+System.out.println (c <= d);
+
+
+    boolean e = true;
+    boolean f = false;
+System.out.println (e && f);
+System.out.println (e || f);
+System.out.println (!e);
+
+
+    int g = 10;
+
+    g+=5;
+    g-=5;
+    g/=5;
+    g*=5;
+    g%=5;
+
     }
 }
